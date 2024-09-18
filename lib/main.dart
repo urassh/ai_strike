6080dc823659dcfaa249eb1e1cab747930bee492
urassh <shuto.1011@icloud.com>
2024-09-18 17:07:04 +0900

    add: list viewのスタイリングをした

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ai_strike/view/ThemeListView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'model/theme/generateThemeImpl.dart';
@@ -18,54 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FutureBuilder(
-          future: GenerateThemeImpl().generalTheme(), // GenerateThemeImplを呼び出す
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator(); // データ取得中のインジケータ
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}'); // エラーメッセージ表示
-            } else {
-              return Text(
-                'Generated Theme: ${snapshot.data?.title ?? "No theme"}', // 生成されたテーマを表示
-                style: Theme.of(context).textTheme.headlineMedium,
-              );
-            }
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // 必要に応じて追加機能を実装
-          });
-        },
-        tooltip: 'Refresh',
-        child: const Icon(Icons.refresh),
-      ),
+      home: const ThemeListView(),
     );
   }
 }
