f6bf12b9e91886b5ebef36eca99600b05306b792
urassh <shuto.1011@icloud.com>
2024-09-20 16:50:17 +0900

    refactor: view

---
+++ lib/view/list/ThemeListCell.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ThemeListCell extends StatelessWidget {
+  final Theme theme;
+
+  const ThemeListCell({super.key, required this.theme});
+
+  final TextStyle _titleStyle = const TextStyle(
+    color: Colors.black87,
+    fontSize: 18,
+    fontWeight: FontWeight.w900,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Color(0xFFA9C9FF),
+            Color(0xFFFFBBEC),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(
+          color: Colors.white.withAlpha(100),
+          width: 4.0,
+        ),
+      ),
+      child: ListTile(
+        leading: const Icon(Icons.label, color: Colors.white), // Change icon color if needed
+        title: Text(
+          'サンプルのお題',
+          style: _titleStyle,
+        ),
+        subtitle: const Text(
+          '2022/12/31 23:59',
+          style: TextStyle(color: Colors.grey, fontSize: 12),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/list/ThemeListView.dart
+++ lib/view/list/ThemeListView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ThemeListCell.dart';
 
 class ThemeListView extends StatelessWidget {
   const ThemeListView({super.key});
@@ -36,7 +37,7 @@ class ThemeListView extends StatelessWidget {
                 itemBuilder: (context, index) {
                   return const Column(
                     children: <Widget>[
-                      _ThemeListCell(),
+                      ThemeListCell(theme: null),
                       SizedBox(height: 8),
                     ],
                   );
@@ -52,49 +53,6 @@ class ThemeListView extends StatelessWidget {
           // Add your onPressed function here
         },
         child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class _ThemeListCell extends StatelessWidget {
-  const _ThemeListCell({super.key});
-
-  final TextStyle _titleStyle = const TextStyle(
-    color: Colors.black87,
-    fontSize: 18,
-    fontWeight: FontWeight.w900,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(
-          colors: [
-            Color(0xFFA9C9FF),
-            Color(0xFFFFBBEC),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-        borderRadius: BorderRadius.circular(12.0),
-        border: Border.all(
-          color: Colors.white.withAlpha(100),
-          width: 4.0,
-        ),
-      ),
-      child: ListTile(
-        leading: const Icon(Icons.label, color: Colors.white), // Change icon color if needed
-        title: Text(
-            'サンプルのお題',
-            style: _titleStyle,
-        ),
-        subtitle: const Text(
-          '2022/12/31 23:59',
-          style: TextStyle(color: Colors.grey, fontSize: 12),
-        ),
       ),
     );
   }
