d43c4d48493aa61f2140032f46f2ad2a3076322c
urassh <shuto.1011@icloud.com>
2024-09-20 15:24:57 +0900

    fix: theme型のプロパティを変更した

--- lib/datamodel/theme.dart
+++ lib/datamodel/theme.dart
@@ -1,9 +1,16 @@
+import 'package:uuid/uuid.dart';
+
 final class Theme {
+  final String id;
+  final DateTime date;
   final String title;
   final String contents;
 
-  const Theme({
+  Theme({
     required this.title,
     required this.contents,
-  });
+    String? id,
+    DateTime? date,
+  })  : id = id ?? const Uuid().v4(),
+        date = date ?? DateTime.now();
 }
