b5028fde124e4650a3b57521be4a06edb722ec8f
urassh <shuto.1011@icloud.com>
2024-09-21 15:33:12 +0900

    delete: 使わなかったpaintを削除

--- lib/view/components/paint/ColorHelper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorHelper {
-  static Color hueToColor(double hueValue) =>
-      HSVColor.fromAHSV(1.0, hueValue, 1.0, 1.0).toColor();
-
-  static double colorToHue(Color color) => HSVColor.fromColor(color).hue;
-}
