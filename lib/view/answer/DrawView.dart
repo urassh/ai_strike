2b83746519d5b237ba4d9644ad5df1c7e9df329b
urassh <shuto.1011@icloud.com>
2024-09-21 15:33:36 +0900

    delete: 余計なimport分を削除

--- lib/view/answer/DrawView.dart
+++ lib/view/answer/DrawView.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../components/GradationContainer.dart';
-import '../components/paint/PaintCanvas.dart'; // ペイントロジックを定義するファイル
 import '../util/AppStyle.dart';
 import 'AnswerViewModel.dart';
 import 'ExplainView.dart';
