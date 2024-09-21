47fe6a0430eb5cc115ecef85bfc0202acba0f7b4
urassh <shuto.1011@icloud.com>
2024-09-21 17:13:34 +0900

    generate Theme Dummyを待機処理

--- lib/model/theme/generateThemeDummy.dart
+++ lib/model/theme/generateThemeDummy.dart
@@ -4,7 +4,8 @@ import 'generateTheme.dart';
 
 class GenerateThemeDummy implements GenerateTheme {
   @override
-  Future<GameTheme> generalTheme() {
+  Future<GameTheme> generalTheme() async {
+    await Future.delayed(const Duration(seconds: 3));
     return Future.value(
         GameTheme.create(title: 'Dummy Theme', contents: 'Dummy Contents'));
   }
