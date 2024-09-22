532520688d342135c305169ed279769c2a802203
urassh <shuto.1011@icloud.com>
2024-09-22 11:09:46 +0900

    画像をはねいさせた

--- lib/view/show/ShowViewModel.dart
+++ lib/view/show/ShowViewModel.dart
@@ -14,9 +14,21 @@ class ShowViewModel extends StateNotifier<List<Answer>> {
   ShowViewModel(super.state);
 
   final FetchAnswers fetchInterface = Firebase();
+  DateTime? _lastFetchTime;
+  bool _hasFetched = false;
 
   Future<void> fetchAnswers(GameTheme theme) async {
+    if (_hasFetched) return; // Prevent multiple fetches
+    _hasFetched = true; // Set this to true once fetching starts
+
+    final currentTime = DateTime.now();
+    if (_lastFetchTime != null && currentTime.difference(_lastFetchTime!).inSeconds < 30) {
+      return;
+    }
+
     final answers = await fetchInterface.fetchAnswers(theme);
     state = answers;
+
+    _lastFetchTime = currentTime;
   }
 }
